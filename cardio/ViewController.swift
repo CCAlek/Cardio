1f562aa165ee00371534564e8138b22fa3991a17
Semen <cemen_torgyy_1993@mail.ru>
2020-11-29 04:22:19 +0900

    Initial Commit

---
+++ cardio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cardio
+//
+//  Created by Семен Семенов on 29.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
